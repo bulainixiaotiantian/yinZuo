ac774565b51062cdbb4cf969ec3826e31bba40b6
bulainixiaotiantian <15515090474@163.com>
2018-06-27 10:42:36 +0800

    first  commit
    first  commit

---
+++ js/tipInfo.js
@@ -0,0 +1,11 @@
+function toast(msg){
+    setTimeout(function(){
+    	document.getElementsByClassName('toast-wrap')[0].style.display = "block";
+        document.getElementsByClassName('toast-wrap')[0].getElementsByClassName('toast-msg')[0].innerHTML=msg;
+        var toastTag = document.getElementsByClassName('toast-wrap')[0];
+        toastTag.className = toastTag.className.replace('toastAnimate','');
+        setTimeout(function(){
+            toastTag.className = toastTag.className + ' toastAnimate';
+        }, 100);
+    },500);
+}
